f13739370f9c7f45d8cd7674b5ca62113e655fc5
houwan <houwan.sek@gmail.com>
2018-08-06 01:08:53 +0800

    some of the work of game logic

--- myDream/Codes/Classes/Card.swift
+++ myDream/Codes/Classes/Card.swift
@@ -12,17 +12,22 @@ import SpriteKit
 class Card: SKNode {
     var value : Int = 0
     var cardNode: SKSpriteNode = SKSpriteNode()
+    var cardBackTextureBlue = SKTexture(imageNamed: "back_blue")
+    var cardBackTextureRed = SKTexture(imageNamed: "back_red")
+    var originTexture: SKTexture?
     var otherValue: Int?
     
     private override init() {
         super.init()
         cardNode = SKSpriteNode.init(imageNamed: "c_A")
+        originTexture = cardNode.texture
         self.addChild(cardNode)
     }
     
     convenience init(imageNamed givenName: String, value givenValue: Int) {
         self.init()
         cardNode.texture = SKTexture(imageNamed: givenName)
+        originTexture = cardNode.texture
         value = givenValue
         if value == 1 {
             otherValue = 11 //just for A
@@ -30,5 +35,36 @@ class Card: SKNode {
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    class func getWholeCards() -> [Card] {
+        var dic = [String:Int]()
+        for pre in ["c","d","h","s"] {
+            for index in 2...10 {
+                let key = pre + "_" + String(index)
+                dic[key] = index
+            }
+            dic[pre + "A"] = 1
+            dic[pre + "J"] = 10
+            dic[pre + "Q"] = 10
+            dic[pre + "K"] = 10
+        }
+        var result = [Card]()
+        for (key,value) in dic {
+            let oneCard = Card(imageNamed: key, value: value)
+            result.append(oneCard)
+        }
+        return result
+    }
+    class func getWholeCards(multiper multi: Int) -> [Card] {
+        var result = [Card]()
+        guard multi >= 1 else {
+            print("multiper must bigger than 1")
+            return result
+        }
+        for _ in 1...multi {
+            let resultTemp = self.getWholeCards()
+            result = result + resultTemp
+        }
+        return result
     }
 }
--- myDream/Codes/SceneMgrs/battleScene.swift
+++ myDream/Codes/SceneMgrs/battleScene.swift
@@ -21,6 +21,11 @@ class battleScene: SKScene,SKButtonDelegate {
                 self.view?.presentScene(scene)
             }
         }
+        else if button == button01 {
+            if button.buttonLabel.text == "Min" {
+                self.decideStake(stake: (house?.minStake)!)
+            }
+        }
     }
     //vars of ui elements
     //naviBar
@@ -34,13 +39,18 @@ class battleScene: SKScene,SKButtonDelegate {
     var button02: SKButton?
     var button03: SKButton?
     var button04: SKButton?
+    
+    //game content layer
+    var betLabel: SKLabelNode?
     //vars of datas
-    //house and relatedFunctionary will be init before scene has been present
+    //house and relatedFunctionary will be init before scene has been present from fightSelectScene
     var house : House?
     var relatedFunctionary: Functionary?
+    var playerInfo = DataManager.shared.getPlayerData()
     
     //vars of game logic
     var logicStatus : gameLogicStatus = .watingForStake
+    var preStake: Int?
     
     override func didMove(to view: SKView) {
 
@@ -53,7 +63,7 @@ class battleScene: SKScene,SKButtonDelegate {
         button02 = self.childNode(withName: "//button02") as? SKButton
         button03 = self.childNode(withName: "//button03") as? SKButton
         button04 = self.childNode(withName: "//button04") as? SKButton
-        
+        betLabel = self.childNode(withName: "//betLabel") as? SKLabelNode
         guard naviBar != nil,
               functionaryNameLabel != nil,
               headIconNode != nil,
@@ -61,6 +71,7 @@ class battleScene: SKScene,SKButtonDelegate {
               button01 != nil,
               button02 != nil,
               button03 != nil,
+              betLabel != nil,
               button04 != nil else {
                 return
               }
@@ -69,12 +80,45 @@ class battleScene: SKScene,SKButtonDelegate {
         let texture = SKTexture(imageNamed: (relatedFunctionary?.imageName)!)
         headIconNode!.texture = texture
         
+        for oneButton in [button01,button02,button03,button04] {
+            oneButton?.target = self
+            oneButton?.disabledTexture = SKTexture(imageNamed: "button_bg_disabled")
+        }
         button01?.buttonLabel.text = "Min"
         button02?.buttonLabel.text = "Max"
         button03?.buttonLabel.text = "Select"
         button04?.buttonLabel.text = "PreTime"
+        if self.preStake == nil {
+            button04?.disabledColorForLabel = UIColor.white
+            button04?.isEnabled = false
+        }
+    }
+    func decideStake(stake givenStake:Int) {
+        guard let playerCurrentDollor = playerInfo?.value(forKey: "currentDollor") as? Int else {
+            print("something wrong to get play's current dollor")
+            return
+        }
+        guard givenStake <= playerCurrentDollor else {
+            print("not enough dollor")
+            return
+        }
+        //buttons of operation layer will move down
+        if let operationLayer = self.childNode(withName: "//operateLayer") {
+            let moveDown = SKAction.move(by: CGVector(dx: 0, dy: -200), duration: 0.3)
+            moveDown.timingMode = .easeInEaseOut
+            operationLayer.run(moveDown)
+        }
+        //change player's dollor
+        let currentDollor = playerCurrentDollor - givenStake
+        playerInfo?.setValue(currentDollor, forKey: "currentDollor")
+        naviBar?.dollorLabel?.text = String(currentDollor)
+        
+        //setup bet label
+        betLabel?.text = "bet :"+" $ " + String(givenStake)
+        betLabel?.isHidden = false
+        
+        
     }
     
-
     
 }
